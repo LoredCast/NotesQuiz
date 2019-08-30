6d02c532d719cd9d9b9a424a9937fa8003059b0d
LoredCast <Loredcastmail@gmail.com>
2019-08-30 17:32:31 +0200

    Added Dic to map Notes to Freqs

--- recording.py
+++ recording.py
@@ -1,7 +1,6 @@
 import pyaudio
 import time
 from itertools import chain
-import wave
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
@@ -12,8 +11,8 @@ CHUNK = 1024
 FORMAT = pyaudio.paFloat32
 CHANNELS = 1
 RATE = 44100
-INTERVAL = 1  # in s
-LIM = 0.12  # Sensitivity
+INTERVAL = 0.5  # in s
+MIN = 0.1  # Sensitivity
 SIZE = INTERVAL * RATE
 
 p = pyaudio.PyAudio()
@@ -29,6 +28,27 @@ x = np.linspace(0.0, 1.0 / (2 * T), int(SIZE / 2))
 
 time = 0
 
+Pitches = [65.4064]
+Root = 65.4064
+for Tone in range(48):
+    Root = Root * 2 ** (1 / 12)
+    Pitches.append(Root)
+
+rootNotes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+Notes = []
+for line in range(4):
+    for e in rootNotes:
+        if line > 0:
+            Notes.append(e + str(line-1))
+        else:
+            Notes.append(e)
+
+
+Tones = dict(zip(Notes, Pitches))
+
+print(Tones)
+
+
 while True:
     frames = []
 
@@ -39,17 +59,17 @@ while True:
     frames = list(chain.from_iterable(frames))
     yr = fft(frames)
     y = 2 / SIZE * np.abs(yr[0:np.int(SIZE / 2)])
+
     freqs = dict(zip(x, y))
     time += INTERVAL
 
-
     for f in freqs:
-        if freqs.get(f) > LIM:
+        if freqs.get(f) > MIN:
             print(f)
     print(time)
 
-#plt.plot(x,y)
-#plt.show()
+# plt.plot(x,y)
+# plt.show()
 
 
 stream.stop_stream()
