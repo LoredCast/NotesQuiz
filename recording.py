9e6b8607c0c4dfcba05e57c0e9d78e98a7a4d10b
LoredCast <Loredcastmail@gmail.com>
2019-09-03 14:11:11 +0200

    changed interval to higher, fft now returns highest amp of intervall, find nearest note based on freq function

--- recording.py
+++ recording.py
@@ -11,7 +11,7 @@ CHUNK = 1024
 FORMAT = pyaudio.paFloat32
 CHANNELS = 1
 RATE = 44100
-INTERVAL = 0.5  # in s
+INTERVAL = 0.75  # in s
 MIN = 0.1  # Sensitivity
 SIZE = INTERVAL * RATE
 
@@ -26,47 +26,65 @@ stream = p.open(format=FORMAT,
 T = 1 / RATE
 x = np.linspace(0.0, 1.0 / (2 * T), int(SIZE / 2))
 
-time = 0
 
-Pitches = [65.4064]
-Root = 65.4064
-for Tone in range(48):
-    Root = Root * 2 ** (1 / 12)
-    Pitches.append(round(Root, 1))
+def gen_scale():
 
-rootNotes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
-Notes = []
-for line in range(4):
-    for e in rootNotes:
-        if line > 0:
-            Notes.append(e + str(line-1))
-        else:
-            Notes.append(e)
+    pitches = [65.4064]
+    root = 65.4064
+
+    for Tone in range(47):
+        root = root * 2 ** (1 / 12)
+        pitches.append(round(root, 1))
+
+    rootnotes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+    names = []
+
+    for line in range(4):
+        for e in rootnotes:
+            if line > 0:
+                names.append(e + str(line - 1))
+            else:
+                names.append(e)
+    return pitches, names
+
+class Notes:
+    def __init__(self, pitches, notes):
+        self.pitches = pitches
+        self.notes = notes
+
+    def find_closest(self, freq):
+        closest = min(self.pitches, key=lambda x: abs(x - freq))
+        Index = self.pitches.index(closest)
+
+        return closest, self.notes[Index]
 
 
-Tones = dict(zip(Pitches, Notes))
+note = Notes(gen_scale()[0], gen_scale()[1])
 
 
+def interval_fft():
+    frames = []
+
+    for i in range(0, int(RATE / CHUNK * INTERVAL)):  # loop through Interval by Chunk
+        data = np.frombuffer(stream.read(CHUNK), 'float32')  # convert data from str to float
+        frames.append(data)
+
+    frames = list(chain.from_iterable(frames))  # flatten list
+    yr = fft(frames)  # raw Fourier Transform
+    y = 2 / SIZE * np.abs(yr[0:np.int(SIZE / 2)])  # only positive values
+
+    return dict(zip(x, y))  # map freqs to amplitude
 
 
 while True:
-    frames = []
+    freqs = interval_fft()
+    loudestFreq = max(freqs, key=freqs.get)
 
-    for i in range(0, int(RATE / CHUNK * INTERVAL)):
-        data = np.frombuffer(stream.read(CHUNK), 'float32')
-        frames.append(data)
+    if loudestFreq > 20:
+        print(round(max(freqs, key=freqs.get), 2))
+    else:
+        print("-")
 
-    frames = list(chain.from_iterable(frames))
-    yr = fft(frames)
-    y = 2 / SIZE * np.abs(yr[0:np.int(SIZE / 2)])
-
-    freqs = dict(zip(x, y))
-    time += INTERVAL
-
-    for f in freqs:
-        if freqs.get(f) > MIN:
-            print(f)
-    print(time)
 
 # plt.plot(x,y)
 # plt.show()
