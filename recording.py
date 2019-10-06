75647bcd2d51d4483362dcbe99fd0b68e65e99e1
LoredCast <43859792+LoredCast@users.noreply.github.com>
2019-10-06 13:01:01 +0200

    Added Prototype Quiz
    There is now an actual quiz

--- recording.py
+++ recording.py
@@ -1,13 +1,9 @@
 import pyaudio
 from random import *
-import os
 import sys
 import time
 from itertools import chain
 import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 from scipy.fftpack import fft
 
 CHUNK = 1024
@@ -15,10 +11,10 @@ FORMAT = pyaudio.paFloat32
 CHANNELS = 1
 RATE = 44100
 INTERVAL = 0.75  # in s
-MIN = 0.1  # Sensitivity
 SIZE = INTERVAL * RATE
 T = 1 / RATE
 SPACE = np.linspace(0.0, 1.0 / (2 * T), int(SIZE / 2))
+SENSITIVITY = 0.003  # which amplitudes get evaluated
 
 p = pyaudio.PyAudio()
 
@@ -28,8 +24,8 @@ stream = p.open(format=FORMAT,
                 input=True,
                 frames_per_buffer=CHUNK)
 
-def gen_scale():
 
+def gen_scale():
     pitches = [65.4064]
     root = 65.4064
 
@@ -55,7 +51,7 @@ class Notes:
         self.notes = notes
 
     def find_closest(self, freq):
-        closest = min(self.pitches, key=lambda x: abs(x - freq))
+        closest        = min(self.pitches, key=lambda x: abs(x - freq))
         index = self.pitches.index(closest)
 
         return closest, self.notes[index]
@@ -65,7 +61,6 @@ note = Notes(gen_scale()[0], gen_scale()[1])
 
 
 def interval_fft():
-
     frames = []
 
     for i in range(0, int(RATE / CHUNK * INTERVAL)):  # loop through Interval by Chunk
@@ -76,7 +71,7 @@ def interval_fft():
     yr = fft(frames)  # raw Fourier Transform
     y = 2 / SIZE * np.abs(yr[0:np.int(SIZE / 2)])  # only positive values
 
-    return dict(zip(SPACE, y))  # map freqs to amplitude
+    return dict(zip(y, SPACE))  # map freqs to amplitude
 
 
 def picknote():
@@ -85,38 +80,37 @@ def picknote():
     return rand_pitch, rand_note
 
 
-
-
 def listen():
-    freqs = interval_fft()
-    loudestFreq = max(freqs, key=freqs.get)
+    fftvals = interval_fft()
+    loudestfreq = 0
+    maxamp = max(fftvals)
+    if maxamp > SENSITIVITY:
+        loudestfreq = fftvals[maxamp]
 
-    if loudestFreq > 25:
-        freq = round(max(freqs, key=freqs.get), 2)
+    if loudestfreq > 25:
+        freq = round(loudestfreq, 2)
         n = note.find_closest(freq)
 
-        return n
+        return freq
     else:
-        return 0, "unsure"
+        return 0
 
 
 def quiz():
     while True:
         playednote = ""
         notepick = picknote()
-        print("Play " + str(notepick[1][1]) + " @ " + str(notepick[1][0])+ "Hz")
+        print("Play " + str(notepick[1][1]) + " @ " + str(notepick[1][0]) + "Hz")
         while playednote != notepick[1][1]:
             rawlisten = listen()
-            playednote = rawlisten[1]
-            #print(playednote, end="\r")
-            sys.stdout.write('\r'+ "You played: " + playednote)
+            playednote = note.find_closest(rawlisten)[1]
+            # print(playednote, end="\r")
+            if rawlisten > 25:
+                sys.stdout.write('\r' + "You most likely played: " + playednote + " @ " + str(rawlisten) + "Hz")
+            else:
+                sys.stdout.write('\r' + "You most likely played: nothing")
         print("\nGood, now next one\n------------------")
-quiz()
 
 
-
-stream.stop_stream()
-stream.close()
-p.terminate()
-
-
+if __name__ == "__main__":
+    quiz()
